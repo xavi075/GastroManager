d8e557c635a4d2056fa192a2ed24812c65977700
ferriculus <ferrancasanovasp@gmail.com>
2024-03-14 18:39:52 +0100

    taules using grid and first version of the responsive layout

--- gastro-manager/src/components/layout.tsx
+++ gastro-manager/src/components/layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState } from 'react';
 import 'tailwindcss/tailwind.css'
 
 interface LayoutProps {
@@ -6,12 +6,33 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="App">
       <header className="items-center w-full text-brown">
         <nav>
-          <a className="flex justify-center text-4xl">Gestió Restaurants</a>
-          <ul className="flex justify-center [&>li>a]:inline-block [&>li>a]:text-xl [&>li>a]:p-3 bg-brown [&>li>a]:text-vanilla rounded-md">
+          <a className="flex justify-center text-4xl sm:text-m">Gestió Restaurants</a>
+          <button
+            className="md:hidden"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+          <ul className={`md:flex md:justify-center md:space-x-4 ${isOpen ? 'block' : 'hidden'}`}>
             <li className="nav-item">
               <a className="transition-colors duration-200 ease-in-out hover:bg-bronze" href="/carta">Carta</a>
             </li>
--- gastro-manager/src/pages/taules.tsx
+++ gastro-manager/src/pages/taules.tsx
@@ -4,17 +4,26 @@ import Layout from '../components/layout';
 const Taules: React.FC = () => {
   return (
     <Layout>
-      <section className="flex flex-col">
+      <section className="flex flex-col w-70 max-w-screen-lg mx-auto p-8">
         <h2 className="text-xl">Taules</h2>
-        <article className="border-1 border-solid border-black border-md p-2.5 mb-4 flex flex-col bg-vanilla text-center transition-colors duration-200 ease-in-out hover:bg-bronze hover:text-white">
-          <h3>Taula 1</h3>
-        </article>
-        <article className="border-1 border-solid border-black border-md p-2.5 mb-4 flex flex-col bg-vanilla text-center transition-colors duration-200 ease-in-out hover:bg-bronze hover:text-white">
-          <h3>Taula 2</h3>
-        </article>
-        <article className="border-1 border-solid border-black border-md p-2.5 mb-4 flex flex-col bg-vanilla text-center transition-colors duration-200 ease-in-out hover:bg-bronze hover:text-white">
-          <h3>Taula 3</h3>
-        </article>
+        <div className="grid gap-4 sm:grid-cols:1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6">
+
+          <article className="border-1 border-solid border-black border-md p-2.5 items-center bg-vanilla text-center transition-colors duration-200 ease-in-out hover:bg-bronze hover:text-white">
+            <h3>Taula 1</h3>
+          </article>
+          <article className="border-1 border-solid border-black border-md p-2.5 items-center bg-vanilla text-center transition-colors duration-200 ease-in-out hover:bg-bronze hover:text-white">
+            <h3>Taula 2</h3>
+          </article>
+          <article className="border-1 border-solid border-black border-md p-2.5 items-center bg-vanilla text-center transition-colors duration-200 ease-in-out hover:bg-bronze hover:text-white">
+            <h3>Taula 3</h3>
+          </article>
+          <article className="border-1 border-solid border-black border-md p-2.5 items-center bg-vanilla text-center transition-colors duration-200 ease-in-out hover:bg-bronze hover:text-white">
+            <h3>Taula 4</h3>
+          </article>
+          <article className="border-1 border-solid border-black border-md p-2.5 items-center bg-vanilla text-center transition-colors duration-200 ease-in-out hover:bg-bronze hover:text-white">
+            <h3>Taula 5</h3>
+          </article>
+        </div>
       </section>
     </Layout>
   );
