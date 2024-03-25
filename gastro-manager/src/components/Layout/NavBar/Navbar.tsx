e20c9c18420b01703715214959bb8b32bb148e09
ferriculus <ferrancasanovasp@gmail.com>
2024-03-25 16:33:48 +0100

    adding theme colors to the navbar

--- gastro-manager/src/components/Layout/NavBar/Navbar.tsx
+++ gastro-manager/src/components/Layout/NavBar/Navbar.tsx
@@ -22,22 +22,14 @@ export const NavBar = () => {
             {/* Logo */}
             <a
               href="#"
-              className="group inline-flex items-center space-x-2 text-lg font-bold tracking-wide text-gray-900 hover:text-gray-600"
+              className="group inline-flex items-center space-x-2 text-xl font-bold tracking-wide text-gray-900 hover:text-gray-600"
             >
-              <svg
-                className="hi-mini hi-cube-transparent inline-block size-5 text-blue-600 transition group-hover:scale-110"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M9.638 1.093a.75.75 0 01.724 0l2 1.104a.75.75 0 11-.724 1.313L10 2.607l-1.638.903a.75.75 0 11-.724-1.313l2-1.104zM5.403 4.287a.75.75 0 01-.295 1.019l-.805.444.805.444a.75.75 0 01-.724 1.314L3.5 7.02v.73a.75.75 0 01-1.5 0v-2a.75.75 0 01.388-.657l1.996-1.1a.75.75 0 011.019.294zm9.194 0a.75.75 0 011.02-.295l1.995 1.101A.75.75 0 0118 5.75v2a.75.75 0 01-1.5 0v-.73l-.884.488a.75.75 0 11-.724-1.314l.806-.444-.806-.444a.75.75 0 01-.295-1.02zM7.343 8.284a.75.75 0 011.02-.294L10 8.893l1.638-.903a.75.75 0 11.724 1.313l-1.612.89v1.557a.75.75 0 01-1.5 0v-1.557l-1.612-.89a.75.75 0 01-.295-1.019zM2.75 11.5a.75.75 0 01.75.75v1.557l1.608.887a.75.75 0 01-.724 1.314l-1.996-1.101A.75.75 0 012 14.25v-2a.75.75 0 01.75-.75zm14.5 0a.75.75 0 01.75.75v2a.75.75 0 01-.388.657l-1.996 1.1a.75.75 0 11-.724-1.313l1.608-.887V12.25a.75.75 0 01.75-.75zm-7.25 4a.75.75 0 01.75.75v.73l.888-.49a.75.75 0 01.724 1.313l-2 1.104a.75.75 0 01-.724 0l-2-1.104a.75.75 0 11.724-1.313l.888.49v-.73a.75.75 0 01.75-.75z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <span>Company</span>
+              <img 
+                src="/images/logo.png" 
+                alt="logo"
+                className="w-16"  
+              />
+              <h1 className="text-blood_red hidden md:block">GastroManager</h1>
             </a>
             {/* END Logo */}
           </div>
@@ -62,11 +54,11 @@ export const NavBar = () => {
               <button
                 onClick={() => setMobileNavOpen(!mobileNavOpen)}
                 type="button"
-                className="inline-flex items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold leading-5 text-gray-800 hover:border-gray-300 hover:text-gray-900 hover:shadow-sm focus:ring focus:ring-gray-300 focus:ring-opacity-25 active:border-gray-200 active:shadow-none"
+                className="inline-flex items-center justify-center space-x-2 rounded-lg border border-blood_red-200 bg-white px-3 py-2 text-sm font-semibold leading-5 hover:border-blood_red-300 hover:text-gray-900 hover:shadow-sm focus:ring focus:border-blood_red-300 focus:ring-opacity-25 active:border-blood_red-500 active:shadow-none"
               >
                 <FontAwesomeIcon 
                   icon={faBars} 
-                  className="hi-solid hi-menu inline-block size-5" 
+                  className={`hi-solid hi-menu inline-block size-5 text-blood_red`}
                 />
               </button>
             </div>
@@ -98,15 +90,19 @@ function NavBarLink( item : NavBarItem) {
   return (
     <Link 
       href={item.path} 
-      className={`group flex items-center space-x-2 rounded-lg border px-3 py-2 text-sm font-medium 
+      className={`group flex items-center space-x-2 rounded-lg border px-3 py-2 text-m font-medium 
         ${pathname === item.path 
-          ? 'border-blue-100 bg-blue-50 text-blue-600' 
-          : 'border-transparent text-gray-800 hover:bg-blue-50 hover:text-blue-600 active:border-blue-100'
+          ? 'border-blood_red-200 bg-blood_red-100 text-blood_red-950 font-extrabold' 
+          : 'border-transparent text-blood_red-900 hover:bg-blood_red-50 hover:text-blood_red-950 active:border-blood_red-100'
       }`}
     >
       <FontAwesomeIcon 
         icon={item.icon} 
-        className="hi-mini hi-briefcase inline-block size-5 opacity-25 group-hover:opacity-100"
+        className={`hi-mini hi-briefcase inline-block size-5 opacity-25  group-hover:opacity-100
+          ${pathname === item.path 
+            ? 'text-blood_red-900'
+            : 'text-blood_red-800'
+        }`}
       />
       <span>{item.label}</span>
     </Link>
--- gastro-manager/src/components/Layout/NavBar/UserDropDown.tsx
+++ gastro-manager/src/components/Layout/NavBar/UserDropDown.tsx
@@ -1,6 +1,6 @@
 import { NavBarItem } from "@/types/types";
 import { Menu, Transition } from "@headlessui/react";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
@@ -8,24 +8,27 @@ import Link from "next/link";
 import { USERDROPDOWN_LINKS } from "../../../../lib/constants/navigation";
 
 export const UserDropDown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <Menu as="div" className="relative inline-block">
       {/* Dropdown Toggle Button */}
-      <Menu.Button className="inline-flex items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-semibold leading-5 text-gray-800 hover:border-gray-300 hover:text-gray-900 hover:shadow-sm focus:ring focus:ring-gray-300 focus:ring-opacity-25 active:border-gray-200 active:shadow-none dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300 dark:hover:border-gray-600 dark:hover:text-gray-200 dark:focus:ring-gray-600 dark:focus:ring-opacity-40 dark:active:border-gray-700">
+      <Menu.Button 
+        className="inline-flex items-center justify-center space-x-2 rounded-lg border border-blood_red-200 text-blood_red px-3 py-2 text-sm font-semibold leading-5 hover:text-blood_red-950 hover:font-extrabold hover:shadow-sm focus:ring focus:ring-gray-300 focus:ring-opacity-25 active:border-gray-200 active:shadow-none"
+        onClick={toggleMenu}
+      >
         <span>John</span>
-        <svg
-          className="hi-mini hi-chevron-down inline-block size-5 opacity-40"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            fillRule="evenodd"
-            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-            clipRule="evenodd"
-          />
-        </svg>
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          className={`hi-mini hi-chevron-down inline-block size-5 transform transition-transform duration-300 ${
+            isOpen ? 'rotate-180' : ''
+          }`}
+        />
+        
       </Menu.Button>
       {/* END Dropdown Toggle Button */}
 
@@ -40,7 +43,7 @@ export const UserDropDown = () => {
         leaveTo="opacity-0 scale-90"
       >
         <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-lg shadow-xl focus:outline-none dark:shadow-gray-900">
-          <div className="divide-y divide-gray-100 rounded-lg bg-white ring-1 ring-black ring-opacity-5 dark:divide-gray-700 dark:bg-gray-800 dark:ring-gray-700">
+          <div className="divide-y divide-gray-100 rounded-lg bg-white ring-1 ring-black ring-opacity-5">
 
             {USERDROPDOWN_LINKS.map((item) => (
               <UserDropDownLink {...item} />
@@ -61,15 +64,11 @@ function UserDropDownLink(item: NavBarItem) {
         {({ active }) => (
           <Link
             href={item.path}
-            className={`group flex items-center justify-between space-x-2 rounded-lg border border-transparent px-2.5 py-2 text-sm font-medium ${
-              active
-                ? "bg-blue-50 text-blue-800 dark:border-transparent dark:bg-gray-700/75 dark:text-white"
-                : "text-gray-700 hover:bg-blue-50 hover:text-blue-800 active:border-blue-100 dark:text-gray-200 dark:hover:bg-gray-700/75 dark:hover:text-white dark:active:border-gray-600"
-            }`}
+            className={`group flex items-center justify-between space-x-2 rounded-lg border border-transparent px-2.5 py-2 text-sm font-medium text-blood_red-900 hover:text-blood_red-950 hover:font-extrabold`}
           >
             <FontAwesomeIcon 
               icon={item.icon} 
-              className="hi-mini hi-chevron-down inline-block size-5 opacity-40" 
+              className="hi-mini hi-chevron-down inline-block size-5 text-blood_red-900" 
             />
 
             <span className="grow">{item.label}</span>
