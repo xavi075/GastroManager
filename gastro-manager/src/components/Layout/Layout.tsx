8e951ce1d09683a4826affcbc31d315c3413c1c7
ferriculus <ferrancasanovasp@gmail.com>
2024-04-05 19:56:49 +0200

    modifications to provide consistency in navbar styles

--- gastro-manager/src/components/Layout/Layout.tsx
+++ gastro-manager/src/components/Layout/Layout.tsx
@@ -13,14 +13,14 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <Providers>
       <div className="App">
-      <header>
-        <NavBar />
-      </header>
-      <main>{children}</main>
-      <footer className="inset-x-0 bottom-0 p-4 text-center">
-        <Footer />
-      </footer>
-    </div>
+        <header>
+          <NavBar />
+        </header>
+        <main>{children}</main>
+        <footer className="inset-x-0 bottom-0 p-4 text-center">
+          <Footer />
+        </footer>
+      </div>
     </Providers>
   );
 };
--- gastro-manager/src/components/Layout/NavBar/Navbar.tsx
+++ gastro-manager/src/components/Layout/NavBar/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 import { UserDropDown } from "./UserDropDown";
 import { NAVBAR_LINKS } from "../../../../lib/constants/navigation";
 import { useRouter } from "next/router";
-import { NavBarItem } from "../../../types/types"; // Ajusta la ruta según la ubicación real del archivo types
+import { NavBarItem } from "../../../types/types";
 
 export const NavBar = () => {
   const [mobileNavOpen, setMobileNavOpen] = useState(false);
@@ -13,7 +13,7 @@ export const NavBar = () => {
   return (
     <div
       id="header"
-      className="z-1 flex flex-none items-center bg-white shadow-sm"
+      className="flex flex-none items-center bg-white shadow-md"
     >
       <div className="container mx-auto px-4 lg:px-8 xl:max-w-7xl">
         <div className="flex justify-between py-4">
@@ -21,13 +21,13 @@ export const NavBar = () => {
           <div className="flex items-center">
             {/* Logo */}
             <a
-              href="#"
+              href="taules"
               className="group inline-flex items-center space-x-2 text-3xl font-bold tracking-wide text-gray-900 hover:text-gray-600"
             >
-              <img 
-                src="/images/logo.png" 
+              <img
+                src="/images/logo.png"
                 alt="logo"
-                className="w-16"  
+                className="w-16"
               />
               <h1 className="text-bronze-900 hidden md:block">GastroManager</h1>
             </a>
@@ -54,11 +54,13 @@ export const NavBar = () => {
               <button
                 onClick={() => setMobileNavOpen(!mobileNavOpen)}
                 type="button"
-                className="inline-flex items-center justify-center space-x-2 rounded-lg border border-bronze-200 bg-white px-3 py-2 text-sm font-semibold leading-5 hover:border-bronze-300 hover:text-gray-900 hover:shadow-sm focus:ring focus:border-bronze-300 focus:ring-opacity-25 active:border-bronze-500 active:shadow-none"
+                className={`group flex items-center space-x-2 rounded-lg border px-3 py-2 text-m font-medium border-bronze-300
+                  ${mobileNavOpen ? 'bg-bronze-100 hover:bg-transparent' : 'bg-transparent hover:bg-bronze-100'}
+                `}
               >
-                <FontAwesomeIcon 
-                  icon={faBars} 
-                  className={`hi-solid hi-menu inline-block size-5 text-bronze-900`}
+                <FontAwesomeIcon
+                  icon={faBars}
+                  className={`hi-solid hi-menu inline-block size-6 text-bronze-900`}
                 />
               </button>
             </div>
@@ -70,7 +72,7 @@ export const NavBar = () => {
         {/* Mobile Navigation */}
         <div className={`lg:hidden ${mobileNavOpen ? "" : "hidden"}`}>
           <nav className="flex flex-col space-y-2 border-t py-4">
-            
+
             {NAVBAR_LINKS.map((item) => (
               <NavBarLink {...item} />
             ))}
@@ -83,26 +85,26 @@ export const NavBar = () => {
   );
 };
 
-function NavBarLink( item : NavBarItem) {
-  const router = useRouter(); // Obtener el objeto router de Next.js
-  const { pathname } = router; // Obtener la ruta actual
+function NavBarLink(item: NavBarItem) {
+  const router = useRouter(); // Obtenir l'objete router de Next.js
+  const { pathname } = router; // Obtenir la ruta actual
 
   return (
-    <Link 
-      href={item.path} 
+    <Link
+      href={item.path}
       className={`group flex items-center space-x-2 rounded-lg border px-3 py-2 text-m font-medium 
-        ${pathname === item.path 
-          ? 'border-bronze-200 bg-bronze-100 text-bronze-950 font-extrabold' 
-          : 'border-transparent text-bronze-900 hover:bg-bronze-50 hover:text-bronze-950 active:border-bronze-100'
-      }`}
+        ${pathname === item.path
+          ? 'border-bronze-200 bg-bronze-100 text-bronze-950 font-extrabold'
+          : 'border-transparent text-bronze-900 hover:bg-bronze-100 hover:text-bronze-950 active:border-bronze-100'
+        }`}
     >
-      <FontAwesomeIcon 
-        icon={item.icon} 
-        className={`hi-mini hi-briefcase inline-block size-5 opacity-25  group-hover:opacity-100
-          ${pathname === item.path 
+      <FontAwesomeIcon
+        icon={item.icon}
+        className={`hi-mini hi-briefcase inline-block size-5 opacity-25 group-hover:opacity-100
+          ${pathname === item.path
             ? 'text-bronze-900'
             : 'text-bronze-800'
-        }`}
+          }`}
       />
       <span>{item.label}</span>
     </Link>
