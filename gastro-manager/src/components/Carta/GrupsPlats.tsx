ae1f725ee049f0637f36c6a48abb282209ece7ea
xavi075 <xm201532@gmail.com>
2024-04-06 17:15:43 +0200

    Add grup edition page and minor styling fixes

--- gastro-manager/src/components/Carta/GrupsPlats.tsx
+++ gastro-manager/src/components/Carta/GrupsPlats.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Plat } from './Plat';
-import { Button } from "@/utils/components"
+import { Button } from "@/utils/components";
+import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faPencil, faFolderPlus } from '@fortawesome/free-solid-svg-icons';
 
@@ -10,7 +11,7 @@ export const GrupsPlats = () => {
             <h2 className='text-2xl font-bold m-2 text-center'>Carta de plats</h2>
             <article className='bg-bronze-200 rounded-md my-4 mx-auto text-center justify-center'>
                 <h3 className='text-2xl font-bold text-center text-gray-800 mb-4 pt-2'>Tapes</h3>
-                <div className='grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4 justify-items-center mx-2'>
+                <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 justify-items-center px-4'>
                     {/* TODO: Substituir la key amb la variable d'id corresponent */}
                     {/* TODO: Iterar i generar automàticament a través d'un mapeig */}
                     <Plat key={"id1"} nom={"Patates Braves"} preu={5.50}/>
@@ -21,28 +22,32 @@ export const GrupsPlats = () => {
                     <Plat key={"id6"} nom={"Patates Xips 4"} preu={1.75}/>
                 </div>
                 {/* TODO: Onclick function */}
-                <button className="bg-brown-600 hover:bg-brown-500 text-white font-bold py-2 px-4 rounded mb-4">
+                {/* <button className="bg-brown-600 hover:bg-brown-500 text-white font-bold py-2 px-4 rounded mx-4 mb-4">
                     Afegir plat <FontAwesomeIcon icon={faPlus}/>
-                </button>
-                <button className="bg-brown-600 hover:bg-brown-500 text-white font-bold py-2 px-4 rounded mb-4 ml-2">
-                    Edita <FontAwesomeIcon icon={faPencil}/>
-                </button>
-            </article>            
+                </button> */}
+                <Link href={"/editaGrupPlats"}>
+                    <button className="bg-brown-600 hover:bg-brown-500 text-white font-bold py-2 px-4 rounded mx-4 mb-4 ml-2">
+                        Edita <FontAwesomeIcon icon={faPencil}/>
+                    </button>
+                </Link>
+            </article>    
             <article className='bg-sky-200 rounded-md my-4 mx-auto text-center justify-center'>
                 <h3 className='text-2xl font-bold text-center text-gray-800 mb-4 pt-2'>Begudes</h3>
-                <div className='grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4 justify-items-center mx-2'>
+                <div className='grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4 justify-items-center px-4'>
                     {/* TODO: Substituir la key amb la variable d'id corresponent */}
                     {/* TODO: Iterar i generar automàticament a través d'un mapeig */}
                     <Plat key={"b1"} nom={"Aigua (50cl)"} preu={1.80}/>
                     <Plat key={"b2"} nom={"Estrella Damm"} preu={2.20}/>
                     <Plat key={"b3"} nom={"Cocacola"} preu={2.30}/>
                 </div>
-                <button className="bg-brown-600 hover:bg-brown-500 text-white font-bold py-2 px-4 rounded mb-4">
+                {/* <button className="bg-brown-600 hover:bg-brown-500 text-white font-bold py-2 px-4 rounded mb-4">
                     Afegir plat <FontAwesomeIcon icon={faPlus}/>
-                </button>
-                <button className="bg-brown-600 hover:bg-brown-500 text-white font-bold py-2 px-4 rounded mb-4 ml-2">
-                    Edita <FontAwesomeIcon icon={faPencil}/>
-                </button>
+                </button> */}
+                <Link href={"/editaGrupPlats"}>
+                    <button className="bg-brown-600 hover:bg-brown-500 text-white font-bold py-2 px-4 rounded mx-4 mb-4 ml-2">
+                        Edita <FontAwesomeIcon icon={faPencil}/>
+                    </button>
+                </Link>
             </article>
 
             <a href="/nueva-pagina" className="block bg-bronze-500 hover:bg-bronze-700 text-white font-bold py-2 px-4 rounded mx-auto text-center">
