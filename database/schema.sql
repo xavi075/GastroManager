5a2adfb3555c2a0d61a406eaac6fdfbc2aab7092
ferriculus <ferrancasanovasp@gmail.com>
2024-04-30 19:14:09 +0200

    procedures and triggers

--- database/schema.sql
+++ database/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE restaurant (
     id INT AUTO_INCREMENT PRIMARY KEY, 
     nom VARCHAR(100) NOT NULL,
     adreca VARCHAR(50) NOT NULL,
-    nif VARCHAR(50) NOT NULL 
+    nif VARCHAR(50) UNIQUE NOT NULL 
 );
 
 CREATE TABLE usuari ( 
@@ -33,14 +33,16 @@ CREATE TABLE taula (
     id INT AUTO_INCREMENT PRIMARY KEY, 
     numTaula INT NOT NULL,
     idRestaurant INT,
-    FOREIGN KEY (idRestaurant) REFERENCES restaurant (id)
+    FOREIGN KEY (idRestaurant) REFERENCES restaurant (id),
+    UNIQUE (numTaula, idRestaurant)
 );
 
 CREATE TABLE grupPlat ( 
     id INT AUTO_INCREMENT PRIMARY KEY, 
     nomGrup VARCHAR(100) NOT NULL,
     idRestaurant INT,
-    FOREIGN KEY (idRestaurant) REFERENCES restaurant (id)
+    FOREIGN KEY (idRestaurant) REFERENCES restaurant (id),
+    UNIQUE (nomGrup, idRestaurant)
 );
 
 CREATE TABLE plat ( 
@@ -48,7 +50,8 @@ CREATE TABLE plat (
     nom VARCHAR(100) NOT NULL,
     preu DECIMAL(5,2) NOT NULL,
     idGrup INT,
-    FOREIGN KEY (idGrup) REFERENCES grupPlat (id)
+    FOREIGN KEY (idGrup) REFERENCES grupPlat (id),
+    UNIQUE (nom, idGrup)
 );
 
 CREATE TABLE menu ( 
@@ -72,12 +75,13 @@ CREATE TABLE comanda (
     dataInici DATETIME NOT NULL,
     dataFi DATETIME,
     idTaula INT,
-    FOREIGN KEY (idTaula) REFERENCES taula (id)
+    FOREIGN KEY (idTaula) REFERENCES taula (id),
+    CHECK (dataInici < dataFi)
 );
 
 CREATE TABLE liniaComanda ( 
     id INT AUTO_INCREMENT PRIMARY KEY, 
-    preuTotal DECIMAL(5,2) NOT NULL,
+    preuTotal DECIMAL(5,2) NOT NULL DEFAULT 0, -- Calculated with a trigger
     quantitat INT NOT NULL DEFAULT 1,
     idComanda INT,
     idPlat INT,
@@ -98,3 +102,126 @@ CREATE TABLE liniaMenu (
     FOREIGN KEY (idSegonPlat) REFERENCES plat (id),
     FOREIGN KEY (idPostres) REFERENCES plat (id)
 );
+
+-- PROCEDURES
+
+-- Procedure per calcular el preu total d'una comanda
+DELIMITER //
+CREATE PROCEDURE calcularPreuTotalComanda(IN p_idComanda INT)
+BEGIN
+    DECLARE total DECIMAL(5,2);
+    
+    -- Calcular el preu total de les línies de comanda
+    SET total = (SELECT SUM(preuTotal) FROM liniaComanda WHERE idComanda = NEW.idComanda);
+    
+    -- Sumar el preu total de les línies de menú
+    SET total = total + 
+        (
+            SELECT SUM(preu) 
+            FROM menu AS tMenu 
+                INNER JOIN liniaMenu AS tLinMenu 
+                    ON tMenu.id = tLinMenu.idMenu
+            WHERE tLinMenu.idComanda = NEW.idComanda
+        );
+    
+    -- Actualizar el precio total en la taula de comanda
+    UPDATE comanda SET preu = total WHERE id = p_idComanda;
+END;
+//  
+DELIMITER ;
+
+
+-- TRIGGERS
+
+-- Trigger per verificar que només existeixi un usuari administrador per restaurant
+DELIMITER //
+CREATE TRIGGER verificarUnicUsuariAdministrador
+BEFORE INSERT ON usuari
+FOR EACH ROW
+BEGIN
+    DECLARE numAdmins INT;
+    SET numAdmins = (SELECT COUNT(*) FROM usuari WHERE idRol = 1 AND idRestaurant = NEW.idRestaurant);
+    IF numAdmins > 0 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Ja existeix un usuari administrador al restaurant';
+    END IF;
+END //
+DELIMITER ;
+
+-- Trigger per calcular el preu total de la linia de comanda abans d'un insert
+DELIMITER //
+CREATE TRIGGER calcularPreuTotalLiniaComanda_BeforeInsert
+BEFORE INSERT ON liniaComanda
+FOR EACH ROW
+BEGIN
+    SET NEW.preuTotal = NEW.quantitat * (SELECT preu FROM plat WHERE id = NEW.idPlat);
+END;
+//
+DELIMITER ;
+
+-- Trigger per calcular el preu total de la linia de comanda abans d'un update
+DELIMITER //
+CREATE TRIGGER calcularPreuTotalLiniaComanda_BeforeUpdate
+BEFORE UPDATE ON liniaComanda
+FOR EACH ROW
+BEGIN
+    SET NEW.preuTotal = NEW.quantitat * (SELECT preu FROM plat WHERE id = NEW.idPlat);
+END;
+//
+DELIMITER ;
+
+-- Trigger per calcular el preu total de la comanda tenint en compte les linies de comanda i les de menu despres d'insert a linia comanda
+DELIMITER //
+CREATE TRIGGER calcularPreuTotalComanda_AfterInsertLiniaComanda
+AFTER INSERT ON liniaComanda
+FOR EACH ROW
+BEGIN
+    CALL calcularPreuTotalComanda(NEW.idComanda);
+END;
+//
+DELIMITER ;
+
+-- Trigger per calcular el preu total de la comanda tenint en compte les linies de comanda i les de menu despres d'update a linia comanda
+DELIMITER //
+CREATE TRIGGER calcularPreuTotalComanda_AfterUpdateLiniaComanda
+AFTER UPDATE ON liniaComanda
+FOR EACH ROW
+BEGIN
+    CALL calcularPreuTotalComanda(NEW.idComanda);
+END;
+//
+DELIMITER ;
+
+-- Trigger per calcular el preu total de la comanda tenint en compte les linies de comanda i les de menu despres d'eliminar a linia comanda
+DELIMITER //
+CREATE TRIGGER calcularPreuTotalComanda_AfterDeleteLiniaComanda
+AFTER DELETE ON liniaComanda
+FOR EACH ROW
+BEGIN
+    CALL calcularPreuTotalComanda(OLD.idComanda);
+END;
+//
+DELIMITER ;
+
+-- Trigger per calcular el preu total de la comanda tenint en compte les linies de comanda i les de menu despres d'insert a linia menu
+DELIMITER //
+CREATE TRIGGER calcularPreuTotalComanda_AfterInsertLiniaMenu
+AFTER INSERT ON liniaMenu
+FOR EACH ROW
+BEGIN
+    CALL calcularPreuTotalComanda(NEW.idComanda);
+END;
+//
+DELIMITER ;
+
+
+-- Trigger per calcular el preu total de la comanda tenint en compte les linies de comanda i les de menu despres d'eliminar a linia menu
+DELIMITER //
+CREATE TRIGGER calcularPreuTotalComanda_AfterDeleteLiniaMenu
+AFTER DELETE ON liniaMenu
+FOR EACH ROW
+BEGIN
+    CALL calcularPreuTotalComanda(OLD.idComanda);
+END;
+//
+DELIMITER ;
+
