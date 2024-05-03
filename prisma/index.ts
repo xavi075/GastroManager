d2fa2b1e11d27aa3e8f95eb2100846cc5910979c
arnauparcerisa <arnau.parcerisa@gmail.com>
2024-05-03 23:37:48 +0200

    Arreglat endpoint get de test

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,30 +1,32 @@
 const { PrismaClient } = require('@prisma/client');
+const { NextApiRequest, NextApiResponse } = require('next');
 const express = require('express');
 const app = express();
 
 
+
 const prisma = new PrismaClient();
 
-app.get('/rols', async (req:any, res:Express.Response) => {
+app.get('/rols', async (req: typeof NextApiRequest, res: typeof NextApiResponse) => {
   try {
     const allRols = await prisma.rol.findMany()
     res.json(allRols)
-    }
-    catch (e: any) {
+  }
+  catch (e: any) {
     res.json({ error: e.message })
-    }
+  }
 })
 
 // async function main() {
 //   // TODO: Escriure Prisma Client queries
 //   const crearRol = await prisma.rol.create({
 //     data: {
-//       nomRol: 'Administrador',
+//       nomRol: 'Cuiner',
 //     },
 //   })
     
 //   const allRols = await prisma.rol.findMany()
-//   // console.log(allRols)
+//   console.log(allRols)
 // }
 
 // main()
@@ -40,3 +42,6 @@ app.get('/rols', async (req:any, res:Express.Response) => {
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
 })
+
+
+
