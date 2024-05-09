4d22f8468531ec9c529b4ec5ba02ed1a9ff79411
ferriculus <ferrancasanovasp@gmail.com>
2024-05-09 18:23:15 +0200

    endpoints to manage the roles using Next API routes

---
+++ src/pages/api/rols/get.ts
@@ -0,0 +1,21 @@
+// pages/api/rols/get.js
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method === "GET") {
+        try {
+            const roles = await prisma.rol.findMany();
+            res.status(200).json(roles);
+        } catch (err: any) {
+            res.status(500).json({ error: err.message });
+        }
+    } else {
+        res.status(405).json({ error: "Method not allowed" });
+    }
+}
