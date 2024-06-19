021b59c618f0c6e22401a370d06e611e611298c6
xavi075 <xm201532@gmail.com>
2024-06-19 23:00:26 +0200

    Modify get.ts

--- src/pages/api/grupplats/get.ts
+++ src/pages/api/grupplats/get.ts
@@ -4,36 +4,14 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "GET") {
         try {
             const { idRestaurant } = req.query;
             const { idGrup } = req.query;
             console.log("Id Restaurant", idRestaurant, " idGrup", idGrup)
-            
-            if (idRestaurant) {
-                let grupsPlats;
-                if (idGrup) {
-                    grupsPlats = await prisma.grupPlat.findUnique({
-                        where: {
-                            id: Number(idGrup),
-                            idRestaurant: Number(idRestaurant),
-                        },
-                        include: {
-                            plat: true,
-                        },
-                    });
-                } else {
-                    grupsPlats = await prisma.grupPlat.findMany({
-                        where: {
-                            idRestaurant: Number(idRestaurant),
-                        },
-                        include: {
-                            plat: true,
-                        },
-                    });
-                }
-              } else if (idRestaurant && idGrup != undefined && idGrup != null) {
+            if (idRestaurant && idGrup != undefined && idGrup != null) {
                 console.log("Obtenir un sol grup")
                 const grupPlat = await prisma.grupPlat.findMany({
                     where: {
@@ -46,16 +24,16 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
                 });
                 res.status(200).json(grupPlat);
                 return;
-              } else if (idRestaurant) {
-                  console.log("Obtenir tots els grups")
-                  const grupsPlats = await prisma.grupPlat.findMany({
-                      where: {
-                          idRestaurant: Number(idRestaurant),
-                      },
-                      include: {
-                          plat: true,
-                      },
-                  });
+            } else if (idRestaurant) {
+                console.log("Obtenir tots els grups")
+                const grupsPlats = await prisma.grupPlat.findMany({
+                    where: {
+                        idRestaurant: Number(idRestaurant),
+                    },
+                    include: {
+                        plat: true,
+                    },
+                });
                 res.status(200).json(grupsPlats);
                 return;
             } else {
